7e992ac6e265ff9bd88fc372655e326d4555057a
Brian Bergeron <bergeron@cmu.edu>
2014-10-19 18:12:18 -0400

    schema formatting

--- main.go
+++ main.go
@@ -221,9 +221,10 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 
-/* SCHEMA = secret VARCHAR(16),
-			encryptedtext VARCHAR(43688),
-			dt DATETIME
+/*  Schema:			
+		secret VARCHAR(16),
+		encryptedtext VARCHAR(43688),
+		dt DATETIME
 */
 func connectDb() (*sql.DB, error){
 

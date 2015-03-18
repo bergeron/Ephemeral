3ec11437d724f388b47768f48c5b4ba41bac4ae4
Brian Bergeron <bergeron@cmu.edu>
2015-03-18 01:33:00 -0400

    preserve newlines

--- static/decrypt.js
+++ static/decrypt.js
@@ -5,7 +5,7 @@ function decrypt(password){
 	try {
 		var key = CryptoJS.PBKDF2(password, salt, { keySize: 128/32 }).toString();
 		var decrypted = CryptoJS.AES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
-		document.getElementById("decryptedMessage").innerHTML = decrypted;
+		document.getElementById("decryptedMessage").innerHTML = decrypted.split("\n").join("<br>");
 	} catch (e) {console.log(e)}	/* Error when incorrect key doesn't generate UTF8 */
 
 	return false;

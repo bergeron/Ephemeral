43aa31eddab5fdc35be910bc8728b7ec03f536ed
unknown <bergeron@cmu.edu>
2016-01-17 01:12:32 -0500

    backend

--- main.go
+++ main.go
@@ -3,13 +3,8 @@
 package main
 import (
 	"net/http"
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"encoding/hex"
 	"strings"
@@ -20,8 +15,11 @@ import (
 	"time"
 	"strconv"
 	"database/sql"
+    "bytes"
+    "encoding/json"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/gorilla/websocket"
+    "golang.org/x/crypto/openpgp"
 )
 
 var db *sql.DB = connectDb()
@@ -29,10 +27,8 @@ var db *sql.DB = connectDb()
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about/", aboutHandler)
-	http.HandleFunc("/create/server/", createServerHandler)
-	http.HandleFunc("/create/client/", createClientHandler)
-	http.HandleFunc("/view/server/", viewServerHandler)
-	http.HandleFunc("/view/client/", viewClientHandler)
+	http.HandleFunc("/send/", sendHandler)
+    http.HandleFunc("/refresh/", refreshHandler)
 	http.HandleFunc("/chat/", chatHandler)
 	http.HandleFunc("/chat/create/", createChatHandler)
 	http.HandleFunc("/chat/ws", serveWs)
@@ -42,9 +38,9 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	/* SSL/TLS */
-	path_to_certificate := "/etc/nginx/ssl/ephemeral/concat_server_and_CA_certs.pem"
-	path_to_key := "/etc/nginx/ssl/ephemeral/private.key"
-	err := http.ListenAndServeTLS(":11994", path_to_certificate, path_to_key, nil)
+	//path_to_certificate := "/etc/nginx/ssl/ephemeral/concat_server_and_CA_certs.pem"
+	//path_to_key := "/etc/nginx/ssl/ephemeral/private.key"
+	err := http.ListenAndServe(":11994", nil)
     if err != nil {
         log.Fatal(err)
     }
@@ -105,132 +101,33 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "about", nil)
 }
 
-/* Given a random 128 bits, encrypt text with an AES cipher */
-func encrypt(key, text []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
+
+/* POST /send/ */
+func sendHandler(w http.ResponseWriter, r *http.Request) {
+
+	ct := r.FormValue("ct")
+    toPubStr := r.FormValue("toPubStr")
+    fromPubStr :=  r.FormValue("fromPubStr")
+
+
+    expireMinutes, err := strconv.Atoi(r.FormValue("expireMinutes"))
 	if err != nil {
-		return nil, err
-	}
-
-	b := base64.StdEncoding.EncodeToString(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
-	return ciphertext, nil
-}
-
-/* Given encrypted text and the AES cipher key, decrypt the text */
-func decrypt(key, text []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	if len(text) < aes.BlockSize {
-		return nil, errors.New("ciphertext too short")
-	}
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-	data, err := base64.StdEncoding.DecodeString(string(text))
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-/* POST /create/server */
-func createServerHandler(w http.ResponseWriter, r *http.Request) {
-
-	text := r.FormValue("text")
-	if len(text) > 16000{
-		writeError(w, "Message too long. Max character length is 16000.")
-		return
-	}
-
-	msgId := generateTableId(db, "messages")
-
-	/* Generate 128 bit key */
-	key128bits := make([]byte, 16)
-	_, err := rand.Read(key128bits)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	/* Encrypt the text */
-	encryptedtextBytes, err := encrypt(key128bits, []byte(text))
-	if err != nil {
-		log.Fatal(err)
-	}
-	encryptedtext := hex.EncodeToString(encryptedtextBytes)
-
-	/* Set expiration date */
-	expireMinutes, err := strconv.Atoi(r.FormValue("expireMinutes"))
-	if err != nil {
-		expireMinutes = 43200	/* Default expire in 30 days */
-	}
+		expireMinutes = 4320
+	} 
+    
 
 	/* Insert message into db */
-	_, err = db.Exec("insert into messages values (?, ?, ?, ?, ?, ?)", 
-						msgId, encryptedtext, nil, time.Now().Unix(), expireMinutes, true)
+ 	_, err = db.Exec("insert into messages values (?, ?, ?, ?, ?)", 
+                        ct, fromPubStr, toPubStr, time.Now().Unix(), expireMinutes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	format := r.FormValue("format")
-	if format == "url" {	/* Return only url */
-		url := "https://" + r.Host + "/view/server/" +msgId + "/" + hex.EncodeToString(key128bits)
-		w.Write([]byte(url))
-	} else {	/* Return html */
-		
-		type Out struct {
-			Host string
-			SecretString string
-			KeyString string
-		}
-		data := Out{r.Host, msgId, hex.EncodeToString(key128bits)}
-		tmpl := template.Must(template.ParseFiles("static/html/create.html", "static/html/top.html", "static/html/head.html"))
-		tmpl.ExecuteTemplate(w, "create", data)
-	}
+    w.Write([]byte("ok"))
 }
 
-/* POST /create/client */
-func createClientHandler(w http.ResponseWriter, r *http.Request) {
-
-	encryptedText := r.FormValue("text")
-	if len(encryptedText) > 16000{
-		writeError(w, "Message too long. Max character length is 16000.")
-		return
-	}
-
-	msgId := generateTableId(db, "messages")
-	salt := r.FormValue("salt")
-
-	/* Set expiration date */
-	expireMinutes, err := strconv.Atoi(r.FormValue("expireMinutes"))
-	if err != nil {
-		expireMinutes = 43200	/* Default expire in 30 days */
-	}
-
-	/* Insert message into db */
-	_, err = db.Exec("insert into messages values (?, ?, ?, ?, ?, ?)", 
-						msgId, encryptedText, salt, time.Now().Unix(), expireMinutes, false)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	url := "https://" + r.Host + "/view/client/" + msgId
-	w.Write([]byte(url))
-}
-
-/* GET /view/server */
-func viewServerHandler(w http.ResponseWriter, r *http.Request) {
-
-	/* Blacklist sites that GET the url before sending to recipient */
+/* GET /refresh/ */
+func refreshHandler(w http.ResponseWriter, r *http.Request) {
 	blacklist := [...]string{"facebook"}
 	
 	for _,e := range blacklist {
@@ -240,125 +137,77 @@ func viewServerHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	/* get query params */
-	queryString := strings.TrimSuffix(r.URL.Path[len("/view/server/"):],"/")
-	params := strings.Split(queryString, "/")
-	if len(params) != 2 {
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
-
-	msgId := params[0]
-	keyString := params[1]
-	keyBytes, err := hex.DecodeString(keyString)
-	if err != nil {
-		fmt.Println("Key is not hex")
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
-
+	pubStr := r.FormValue("pubStr")
+    signature := r.FormValue("signature")
+    
+    if !verifySignature(pubStr, signature){
+        w.Write([]byte("failed"))
+        return
+    }
+	
 	var m sync.Mutex
 	m.Lock() /* ONLY ONE THREAD IN HERE AT A TIME */
 
-	/* Lookup message in db */
-	var encryptedText string
-	err = db.QueryRow("SELECT encrypted_text FROM messages WHERE id = ?", msgId).Scan(&encryptedText)
+    rows, err := db.Query("SELECT ct FROM messages WHERE to_pub_str = ?", pubStr)
 	if err != nil {
-		fmt.Println("No message found with msgId: " + msgId)
-		writeError(w, "Message not found. It may have been deleted.")
-		return
+		fmt.Println(err.Error())
+	}
+    if rows.Err() != nil {
+		fmt.Println(err.Error())
 	}
 
-	/* Decrypt message */
-	encryptedtextBytes , err := hex.DecodeString(encryptedText)
-	if err != nil {
-		fmt.Println("Error converting encryptedext from string to byte[]")
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
-	messageBytes, err := decrypt(keyBytes, []byte(encryptedtextBytes))
-	if err != nil {
-		fmt.Println("Valid msgId, but invalid key")
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
+	messages := []string{}
 
-	message := template.HTMLEscapeString(string(messageBytes))	/* no XSS */
-
-	/* Delete message from db */
-	_, err = db.Exec("DELETE FROM messages WHERE id = ? LIMIT 1", msgId)
+	defer rows.Close()
+    for rows.Next() {
+		var ct string
+		err = rows.Scan(&ct)
+		messages = append(messages, ct)
+	}
+	
+	_, err = db.Exec("DELETE FROM messages WHERE to_pub_str = ?", pubStr)
 	if err != nil {
-		fmt.Println("Message already deleted.")		/* Shouldn't happen */
+        fmt.Println(err.Error())
 	}
 
 	m.Unlock()	/*DONE */
 
-	fmt.Println("Message Found!")
-
-	/* Write HTML */
-	type Out struct {
-		Success bool
-		Message []string
+    type Out struct {
+        Success bool
+		Messages []string
 	}
-	data := Out{true, strings.Split(message, "\n")}
-	tmpl := template.Must(template.ParseFiles("static/html/viewServer.html", "static/html/top.html", "static/html/head.html"))
-	tmpl.ExecuteTemplate(w, "viewServer", data)
+
+    json, err := json.Marshal(Out{true, messages})
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		fmt.Println(err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+
 }
 
-/* GET /view/client */
-func viewClientHandler(w http.ResponseWriter, r *http.Request) {
+func verifySignature(pubStr string, signature string)(bool){
+    msgNoArmor := "Proof I own the key: " + pubStr
 
-	/* Blacklist sites that GET the url before sending to recipient */
-	blacklist := [...]string{"facebook"}
-	
-	for _,e := range blacklist {
-		if strings.Contains(r.UserAgent(), e) {
-			fmt.Fprintf(w, "Go away %s! This is only for the recipient!", e)
-			return
-		}
-	}
-
-	/* get query params */
-	queryString := strings.TrimSuffix(r.URL.Path[len("/view/client/"):],"/")
-	params := strings.Split(queryString, "/")
-	if len(params) != 1 {
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
-	msgId := params[0]
-
-	var m sync.Mutex
-	m.Lock() /* ONLY ONE THREAD IN HERE AT A TIME */
-
-	/* Lookup message in db */
-	var encryptedText string
-	var salt string
-	err := db.QueryRow("SELECT encrypted_text, salt FROM messages WHERE id = ?", msgId).Scan(&encryptedText, &salt)
-	if err != nil {
-		fmt.Println("No message found with msgId: " + msgId)
-		writeError(w, "Message not found. It may have been deleted.")
-		return
-	}
-
-	 /* Delete message from db */
-	_, err = db.Exec("DELETE FROM messages WHERE id = ? LIMIT 1", msgId)
-	if err != nil {
-		fmt.Println("Message already deleted.")		/* Shouldn't happen */
-	}
-
-	m.Unlock()	/*DONE */
-
-	fmt.Println("Message Found!")
-
-	/* Write HTML */
-	type Out struct {
-		Success bool
-		Message string
-		Salt string
-	}
-	data := Out{true, encryptedText, salt}
-	tmpl := template.Must(template.ParseFiles("static/html/viewClient.html", "static/html/top.html", "static/html/head.html"))
-	tmpl.ExecuteTemplate(w, "viewClient", data)
+    keyring, err := openpgp.ReadArmoredKeyRing(bytes.NewBufferString(pubStr))
+    if err != nil {
+            fmt.Printf("failed to parse public key: %s", err)
+    }
+   
+    entity, err := openpgp.CheckArmoredDetachedSignature(keyring, bytes.NewBufferString(msgNoArmor),  bytes.NewBufferString(signature));
+    
+    if err != nil {
+        fmt.Printf("failed to check signature: %s", err)
+        return false
+    } else if keyring[0].PrimaryKey.KeyId == entity.PrimaryKey.KeyId {
+        fmt.Println("VERIFIED")
+        return true
+    }
+    
+    return false
 }
 
 /* Generate unique 128 random bits */
